818bb73f6c0e7fb4c2512fadc881266a3c43fb79
Jacky Luo <97552342+j4ckyluo@users.noreply.github.com>
2023-03-10 14:10:39 -0500

    Update samples.py

--- src/backend/database/samples.py
+++ src/backend/database/samples.py
@@ -1,11 +1,15 @@
 from database import*
 
 #creating a user
-create_student("testID", "fds", "testpassword","123456789", "j_luo3@uncg.edu")
+create_student("John", "Smith", "testpassword","123456789", "j_luo3@uncg.edu")
 
 #Hard-coding administrator accounts
-create_admin("Jacky", "Luo", "jackyluo123","884594913", "j_luo3@uncg.edu")
-create_admin("Luke", "Brown", "lukebrown123","8883198384", "lcbrown9@uncg.edu")
+create_admin("Jacky", "Luo", "jacky123","884594913", "j_luo3@uncg.edu")
+create_admin("Luke", "Brown", "luke123","8883198384", "lcbrown9@uncg.edu")
+create_admin("Jordan", "Barksdale", "jordan123","874594123", "jkbarksd@uncg.edu")
+create_admin("Jesea", "Littlejohn", "jesea123","888319832", "jalittl5@uncg.edu")
+create_admin("Jason", "Martinez-Jimenez", "jason123","884591234", "j_mart24@uncg.edu")
+create_admin("Devon", "Loy", "devon123","8883198384", "d_loy@uncg.edu")
 
 # grabbing all the users.
 # for user in get_all_users():
