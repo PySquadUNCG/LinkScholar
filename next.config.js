e8b296d2871242c6f53d2d6c1b9dbc6cfdc7ec09
Devon A Loy <dloy3000@gmail.com>
2023-03-12 22:55:06 -0400

    Added Next.JS->Flask rewrites for test functions and API calls made from Node server.

--- next.config.js
+++ next.config.js
@@ -2,8 +2,20 @@ module.exports = () => {
     const rewrites = () => {
         return [
             {
-                source: "/hello/:path*",
-                destination: "http://localhost:5000/hello/:path*",
+                source: "/testHello/:path*",
+                destination: "http://localhost:5000/testHello/:path*",
+            },
+            {
+                source: "/testPrompt/:path*",
+                destination: "http://localhost:5000/testPrompt/:path*",
+            },
+            {
+                source: "/testParams:path*",
+                destination: "http://localhost:5000/testParams",
+            },
+            {
+                source: "/testMultiRouter/:path*",
+                destination: "http://localhost:5000/testMultiRouter/:path*",
             },
         ];
     };
