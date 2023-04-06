35e355e43b8e74dba5e6229acd159cd9e3779076
Devon A Loy <dloy3000@gmail.com>
2023-04-06 16:58:20 -0400

    Updated /api/post/account/ routes to return data along with a status.

--- src/backend/api/router.py
+++ src/backend/api/router.py
@@ -167,11 +167,11 @@ def postReqAccount(field):
         data = request.get_json()
 
         try:
-            postReqRouter[field](
+            query = postReqRouter[field](
                 str(data['email']),
                 str(data['password'])
             )
-            return createResponse(content=jsonify({"Status": "Success"}), status=200, corsHeaders="POST,OPTIONS")
+            return createResponse(content=jsonify({"Status": "Success", "Data": query }), status=200, corsHeaders="POST,OPTIONS")
         except TypeError:
             return createResponse(content=jsonify({"Error": "The requested data could not be fetched due to a type mismatch."}), status=500)
         except KeyError:
