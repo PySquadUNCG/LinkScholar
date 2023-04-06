55bdf871d315e2d3acba15ab1cb461aa5ae23f39
Devon A Loy <dloy3000@gmail.com>
2023-04-06 16:58:37 -0400

    Fixed error with login page.

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link';
 import Head from 'next/head';
 import LinkScholarAPI from '../backend/api/API';
@@ -9,14 +9,11 @@ const LogInPage = () => {
   const router = useRouter();
 
   const [formData, setFormData] = useState({
-    studentID: '',
+    email: '',
     password: '',
 
   });
   const [formFilled, setFormFilled] = useState(false);
-
-
-  
 
   const handleInputChangelog = (event) => {
     const { name, value } = event.target;
@@ -27,67 +24,65 @@ const LogInPage = () => {
   };
 
   const handleSubmit = async (event) => {
-    
-    const {response,loaded} = await LinkScholarAPI("/api/get/user/", "schoolID", {param: formData.studentID});
-      console.log(response);
+    console.log('Form submitted!');
+    event.preventDefault();
 
-      if(formData.studentID===9){
-     
+    const { response, loaded } = await LinkScholarAPI("/api/post/account/", "login", { email: formData.email, password: formData.password }, "POST");
+    console.log(response);
+
+    if (response["Data"] === true) {
       console.log('Form submitted:', formData);
-      event.preventDefault(); 
+      event.preventDefault();
       router.push("/homepage");
-    }else {
+    } else {
       alert("Id/Password is Incorrect, Try Again!");
     }
-    
   };
 
-
-
- // Check if all form inputs have a value
+  // Check if all form inputs have a value
   useEffect(() => {
- 
-      const isFormFilled = Object.values(formData).every((value) => value !== '');
-      setFormFilled(isFormFilled);
-   
-     
+
+    const isFormFilled = Object.values(formData).every((value) => value !== '');
+    setFormFilled(isFormFilled);
+
+
   }, [formData]);
-  
+
 
   return (
 
-  <>
- <div className='jb'>
-   
-    <div class="topnav">
-    <a class="active" >Login</a>
-    <Link href="/signin">Sign Up</Link>
-    </div>
+    <>
+      <div className='jb'>
 
-  <Head>
-    <title>Linkscholar-Login</title>
-  </Head> 
- 
-  
-    <div class="login-box">
-      <div class = "icon"><img src="/LinkScholar.png" alt="My Image" /></div>
-      <form onSubmit={handleSubmit}>
-        <label>Enter ID</label>
-        <input type="number" id = "studentID" name = "studentID" value = {formData.studentID} onChange={handleInputChangelog}  required />
+        <div class="topnav">
+          <a class="active" >Login</a>
+          <Link href="/signin">Sign Up</Link>
+        </div>
 
-        <label>Password</label>
-        <input type="password" id = "password" name = "password" value={formData.password} onChange={handleInputChangelog} required  />
-       
-        <button className='submit' type="submit" disabled={!formFilled} > Login </button>
-            
+        <Head>
+          <title>Linkscholar-Login</title>
+        </Head>
 
-      </form>
-      <Link className = "indent" href="/signin">Need An Account? Sign Up Here</Link> <br></br>
-      <Link className = "indent-2" href="/forgotusrname">Forgot username or password?</Link>
-      
-       </div>  
-  </div>
-  </>
+
+        <div class="login-box">
+          <div class="icon"><img src="/LinkScholar.png" alt="My Image" /></div>
+          <form onSubmit={handleSubmit}>
+            <label>Enter Email</label>
+            <input type="string" id="email" name="email" value={formData.email} onChange={handleInputChangelog} required />
+
+            <label>Password</label>
+            <input type="password" id="password" name="password" value={formData.password} onChange={handleInputChangelog} required />
+
+            <button className='submit' type="submit" disabled={!formFilled} > Login </button>
+
+
+          </form>
+          <Link className="indent" href="/signin">Need An Account? Sign Up Here</Link> <br></br>
+          <Link className="indent-2" href="/forgotusrname">Forgot username or password?</Link>
+
+        </div>
+      </div>
+    </>
 
   )
 }
