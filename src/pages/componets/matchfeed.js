0bb665dcb32756953e79e8a0bb93b2ee7155f0dd
Jkbarksd <jkbarksd@uncg.edu>
2023-04-20 17:54:46 -0400

    fix for matchfeed

--- src/pages/componets/matchfeed.js
+++ src/pages/componets/matchfeed.js
@@ -4,32 +4,40 @@ import LinkScholarAPI from '../../backend/api/API';
 import React, { useEffect, useState } from "react";
 
 const MatchFeed = ({ email, school_id }) => {
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearchInputChange = (event) => {
     setSearchQuery(event.target.value);
   }
 
-  const getPostsData = (users) => {
-    if (!users) {
-      return [];
-    }
-
-    return users.slice(0, 5).map((user) => ({
-      authorFirstName: user.first_name,
-      authorLastName: user.last_name,
-      content: user.email
-    }));
-  };
-
-  const postsData = getPostsData(users);
-
   const getMatches = async () => {
-    const { response, loaded } = await LinkScholarAPI("/api/get/match/", "getUserMatch",{param: school_id}, "GET");
-    if (Array.isArray(response)) {
+    const { response: schoolIds, loaded } = await LinkScholarAPI("/api/get/match/", "getUserMatch", { param: school_id }, "GET");
+
+    if (Array.isArray(schoolIds)) {
+      const users = [];
+
+      for (const id of schoolIds) {
+        const { response, loaded } = await LinkScholarAPI("/api/get/user/", "schoolID", { param: id }, "GET");
+        console.log("response:",response[0]);
+        if (response) {
+          const formattedUser = {
+            first_name: response[0].first_name,
+            last_name: response[0].last_name,
+            email: response[0].email
+          };
+          console.log("formattedUser:", formattedUser);
+          users.push(formattedUser);
+
+          if (users.length === 5) {
+            break;
+          }
+        }
+      }
+      console.log("returned matches: ", users);
+
       // Filter the response based on the search query
-      const filteredUsers = response.filter((user) => {
+      const filteredUsers = users.filter((user) => {
         const fullName = `${user.first_name} ${user.last_name}`;
         const lowerCaseSearchQuery = searchQuery.toLowerCase();
         return (
@@ -37,43 +45,45 @@ const MatchFeed = ({ email, school_id }) => {
           user.email.toLowerCase().includes(lowerCaseSearchQuery)
         );
       });
+
       setUsers(filteredUsers);
     }
   };
   
-
   useEffect(() => {
     getMatches();
   }, []);
 
+  
   return (
     <MatchLayout>
-      <h1 className="your-matches">Your Matches:</h1>
-      <div className="search-container">
-        <input
-          className='match-input'
-          type="text"
-          value={searchQuery}
-          onChange={handleSearchInputChange}
-          placeholder="Search by name or email"
+    <h1 className="your-matches">Your Matches:</h1>
+    <div className="search-container">
+      <input
+        className='match-input'
+        type="text"
+        value={searchQuery}
+        onChange={handleSearchInputChange}
+        placeholder="Search by name or email"
+      />
+      <button className='match-input-search' onClick={getMatches}>Search</button>
+    </div>
+    {users.length > 0 ? (
+      users.map(({ first_name, last_name, email }, index) => (
+        <MatchPost
+          key={index}
+          author={`${first_name} ${last_name}`}
+          content={email}
         />
-        <button className='match-input-search' onClick={getMatches}>Search</button>
-      </div>
-      {users && users.length > 0 ? (
-        postsData.map((post, index) => (
-          <MatchPost
-            key={index}
-            author={`${post.authorFirstName} ${post.authorLastName}`}
-            content={post.content}
-          />
-        ))
-      ) : (
-        <p className='no-matches-yet'>No Matches Yet</p>
-      )}
-      <button className="match-refresh" onClick={getMatches}>
-        refresh
-      </button>
-    </MatchLayout>
+      ))
+    ) : (
+      <p className='no-matches-yet'>No Matches Yet</p>
+    )}
+
+    <button className="match-refresh" onClick={getMatches}>
+      refresh
+    </button>
+  </MatchLayout>
   );
 };
 
